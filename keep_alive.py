439dad1cca528488df8387e7b51845fa2e3efaf7
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-24 00:50:21 +0330

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from threading import Thread
+import requests
+import time
 
 app = Flask('')
 
@@ -10,6 +12,16 @@ def home():
 def run():
     app.run(host='0.0.0.0', port=8080)
 
+def ping():
+    while True:
+        try:
+            requests.get("https://xbegoo.onrender.com")
+        except:
+            pass
+        time.sleep(600)  # هر 10 دقیقه
+
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    t1 = Thread(target=run)
+    t1.start()
+    t2 = Thread(target=ping)
+    t2.start()

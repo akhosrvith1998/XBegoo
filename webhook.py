b7df67a26898a360969f51d72bbddbde42846c75
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-27 13:04:47 +0330

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,10 +2,11 @@ from flask import Flask, request, Response
 import requests
 import os
 from main import process_update
+from reply_whisper import process_reply_whisper
 from logger import logger
 
 app = Flask(__name__)
-TOKEN = "7672898225:AAHymEtVaPhC9SbKKSjCaRlkPx68S4ujLEc"
+TOKEN = "7844345303:AAGyDzl4oJjm646ePdx0YQP32ARuhWL6qHk"
 URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 @app.route("/webhook", methods=["POST"])
@@ -13,7 +14,10 @@ def webhook():
     try:
         update = request.get_json()
         logger.info("Received update: %s", update)
-        threading.Thread(target=process_update, args=(update,)).start()
+        if "inline_query" in update:
+            threading.Thread(target=process_update, args=(update,)).start()
+        elif "message" in update and "reply_to_message" in update["message"]:
+            threading.Thread(target=process_reply_whisper, args=(update,)).start()
         return Response(status=200)
     except Exception as e:
         logger.error("Webhook error: %s", str(e))
